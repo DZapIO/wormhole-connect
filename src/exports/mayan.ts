4be692aa56e30203482556f1f70d7454e361cd58
Bradley Playdon <61969883+bradleyplaydon@users.noreply.github.com>
2025-08-15 21:19:38 +0100

    feat: Migrate Mayan route into Connect (#3718)
    * feat: Pull in mayan route & fix error overlay from being wiped
    
    * fix: fixed type errors
    
    * feat: Add missing mayan deps
    
    * feat: install solana@web3
    
    * refactor: split out Mayan route types and classes
    
    * fix: rebase fixes
    
    * fix: fixed missing and incorrect imports
    
    * refactor: add Mayan route exports file & remove @mayan-finance/wormhole-sdk-route
    
    * refactor: added default exports for mayan routes
    
    * fix: fixed import casing
    
    * fix: file casing
    
    * refactor: remove MayanRouteBase from export & pin @mayanfinance/swap-sdk, @testnet-mayan/swap-sdk
    
    * refactor: revert back to the exact code mayan-finance/wormhole-sdk-route
    
    to bypass ts & lint issues added ts ignores and eslint disable lines
    
    ---------
    
    Co-authored-by: Brad playdon <bradplaydonwh@Brads-MacBook-Pro.local>

--- src/exports/mayan.ts
+++ src/exports/mayan.ts
@@ -1,8 +1,7 @@
 export {
   MayanRoute,
-  MayanRouteWH,
   MayanRouteMCTP,
-  MayanRouteSWIFT,
   MayanRouteMONOCHAIN,
-  createMayanRouteWithReferrerFee,
-} from '@mayanfinance/wormhole-sdk-route';
+  MayanRouteSWIFT,
+  MayanRouteWH,
+} from 'routes/mayan';
